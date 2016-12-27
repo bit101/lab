968f3187ad63d8e6fc49b15ad5604daead161389
Keith Peters <kp@bit-101.com>
2016-12-27 14:31:28 -0500

    structural set up

--- 2016/161227.js
+++ 2016/161227.js
@@ -1,1 +1,6 @@
-alert("it works");
+var context = bitlib.context(0, 0),
+    width = context.width,
+    height = context.height;
+
+context.fillStyle = bitlib.color.randomRGB();
+context.fillRect(0, 0, width, height);
