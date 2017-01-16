626316f5ec3c7ae442125de2f235f39c5878f643
Keith Peters <kp@bit-101.com>
2017-01-16 13:15:39 -0500

    better templating system for dailies

---
+++ js/ui.js
@@ -0,0 +1,40 @@
+var panel;
+
+function setup(yymmdd, prev, next, desc, issueNumber, links) {
+    document.title = yymmdd;
+    panel= QuickSettings.create(10, 10, yymmdd)
+        .addHTML("home", '<a href="https://bit101.github.io/lab/">CALENDAR</a> - <a href="https://bit101.github.io/lab/thumbs.html">THUMBS</a>')
+        .addHTML("Description", desc);
+
+    if(links) {
+        panel.addHTML("Links", "");
+        for(var i = 0; i < links.length; i++) {
+            panel.addHTML("link" + i, links[i]);
+        }
+    }
+
+    var prevNext = '';
+    if(prev) {
+        prevNext += '<a href="' + prev + '.html">PREV</a>';
+        if(next) {
+            prevNext += " - ";
+        }
+    }
+    if(next) {
+        prevNext += '<a href="' + next + '.html">NEXT</a>'
+    }
+
+    panel
+        .addHTML("source_comments", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=Check it: https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/facebook.png"></a>')
+        .addHTML("prev_next", prevNext)
+        .hideAllTitles()
+        .showTitle("Description");
+
+    if(links) {
+        panel.showTitle("Links");
+    }
+
+    var script = document.createElement("script");
+    script.src = yymmdd + ".js";
+    document.body.appendChild(script);
+}
