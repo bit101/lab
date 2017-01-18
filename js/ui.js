854f909d41a439633d35bedc0ea8d37a85c5a57f
Keith Peters <kp@bit-101.com>
2017-01-17 20:16:43 -0500

    170118

--- js/ui.js
+++ js/ui.js
@@ -24,8 +24,11 @@ function setup(yymmdd, prev, next, desc, issueNumber, links) {
         prevNext += '<a href="' + next + '.html">NEXT</a>'
     }
 
+    var tweets = ["Check it!", "Look!", "Check this out!", "I'm looking at this:", "Click this:", "Today's BIT-101 Lab:", "Have you seen this?", "A thing:", "Hey!"],
+        tweet = tweets[bitlib.random.int(tweets.length)];
+
     panel
-        .addHTML("source_comments", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=Check it: https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/facebook.png"></a>')
+        .addHTML("source_comments", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=' + tweet +  ' https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/facebook.png"></a>')
         .addHTML("prev_next", prevNext)
         .hideAllTitles()
         .showTitle("Description");
