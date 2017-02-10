bcb3a8482573f88d0dca8fd63b4b0a68a27d306f
Keith Peters <kp@bit-101.com>
2017-02-10 09:13:15 -0500

    changed panel layout to make it more useable

--- js/ui.js
+++ js/ui.js
@@ -3,15 +3,7 @@ var panel;
 function setup(yymmdd, prev, next, desc, issueNumber, links) {
     document.title = yymmdd;
     panel= QuickSettings.create(10, 10, yymmdd)
-        .addHTML("home", '<a href="https://bit101.github.io/lab/">CALENDAR</a> - <a href="https://bit101.github.io/lab/thumbs.html">THUMBS</a>')
-        .addHTML("Description", desc);
-
-    if(links) {
-        panel.addHTML("Links", "");
-        for(var i = 0; i < links.length; i++) {
-            panel.addHTML("link" + i, links[i]);
-        }
-    }
+        .addHTML("home", '<a href="https://bit101.github.io/lab/">CALENDAR</a> - <a href="https://bit101.github.io/lab/thumbs.html">THUMBS</a>');
 
     var prevNext = '';
     if(prev) {
@@ -27,6 +19,15 @@ function setup(yymmdd, prev, next, desc, issueNumber, links) {
     panel
         .addHTML("source_comments", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=Check it: https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html&picture=https://bit101.github.io/lab/thumbs/' + yymmdd + '.png"><img src="../images/facebook.png"></a>')
         .addHTML("prev_next", prevNext)
+        .addHTML("Description", desc);
+
+        if(links) {
+            panel.addHTML("Links", "");
+            for(var i = 0; i < links.length; i++) {
+                panel.addHTML("link" + i, links[i]);
+            }
+        }
+    panel
         .hideAllTitles()
         .showTitle("Description");
 
