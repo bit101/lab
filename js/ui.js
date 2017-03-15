c8eb7f60c76cfdad943be5b990cff9a62ff81a7a
Keith Peters <kp@bit-101.com>
2017-03-14 21:28:21 -0400

    rss feed support

--- js/ui.js
+++ js/ui.js
@@ -17,7 +17,7 @@ function setup(yymmdd, prev, next, desc, issueNumber, links) {
     }
 
     panel
-        .addHTML("Source, Comments, Social", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=Check it: https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html&picture=https://bit101.github.io/lab/thumbs/' + yymmdd + '.png"><img src="../images/facebook.png"></a>')
+        .addHTML("Source, Comments, Social", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=Check it: https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html&picture=https://bit101.github.io/lab/thumbs/' + yymmdd + '.png"><img src="../images/facebook.png"></a><a href="https://bit101.github.io/lab/feed.xml"><img src="../images/rss.png"></a>')
         .addHTML("hide", "H key toggles panel")
         .addHTML("prev_next", prevNext)
         .addHTML("Description", desc);
