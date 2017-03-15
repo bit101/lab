78b340cd32aac6cfef476cfe52c1f17c920ccb6c
Keith Peters <kp@bit-101.com>
2017-03-14 20:11:26 -0400

    quicksettings 3.0.2, bitlib 1.0.9. h key hides panel

--- js/ui.js
+++ js/ui.js
@@ -18,6 +18,7 @@ function setup(yymmdd, prev, next, desc, issueNumber, links) {
 
     panel
         .addHTML("Source, Comments, Social", '<a href="https://github.com/bit101/lab/blob/master/dailies/' + yymmdd + '.js"><img src="../images/github.png"></a><a href="https://github.com/bit101/lab/issues/' + issueNumber + '"><img src="../images/comment.png"></a><a href="https://twitter.com/home?status=Check it: https://bit101.github.io/lab/dailies/' + yymmdd + '.html"><img src="../images/twitter.png"></a><a href="https://www.facebook.com/sharer.php?u=https://bit101.github.io/lab/dailies/' + yymmdd + '.html&picture=https://bit101.github.io/lab/thumbs/' + yymmdd + '.png"><img src="../images/facebook.png"></a>')
+        .addHTML("hide", "H key toggles panel")
         .addHTML("prev_next", prevNext)
         .addHTML("Description", desc);
 
@@ -30,7 +31,8 @@ function setup(yymmdd, prev, next, desc, issueNumber, links) {
     panel
         .hideAllTitles()
         .showTitle("Source, Comments, Social")
-        .showTitle("Description");
+        .showTitle("Description")
+        .setKey("h");
 
     if(links) {
         panel.showTitle("Links");
