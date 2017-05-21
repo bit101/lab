7fbe1443b34a2255b0507140cf9ae4ccc521fe04
Keith Peters <kp@bit-101.com>
2017-05-21 09:21:43 -0400

    170521

---
+++ dailies/170521.js
@@ -0,0 +1,123 @@
+/*
+ Copyright 2017 Keith Peters
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+var context = bitlib.context(0, 0),
+    width = context.width,
+    height = context.height;
+var scale = 0.6;
+var dist = 150;
+var w = 100;
+var h = 100;
+var iter = 4;
+var alpha = 0.5;
+var fill = true;
+var angles = [],
+    num = 4;
+var text = "hello world";
+var textRotation = 0;
+var fontSize = 40;
+for(var i = 0; i < num; i++) {
+    angles.push(bitlib.random.float(Math.PI * 2));
+}
+
+
+panel
+    .addRange("scale", 0, 1, scale, 0.01, function(value) {
+        scale = value;
+    })
+    .addRange("dist", 0, 300, dist, 1, function(value) {
+        dist = value;
+    })
+    .addText("text", text, function(value) {
+        text = value;
+    })
+    .addRange("font size", 10, 100, fontSize, 1, function(value) {
+        fontSize = value;
+    })
+    .addRange("text rotation", 0, Math.PI * 2, 0, 0.01, function(value) {
+        textRotation = value;
+    })
+    .addRange("iter", 1, 7, iter, 1, function(value) {
+        iter = value;
+    })
+    .addRange("alpha", 0, 1, alpha, 0.01, function(value) {
+        alpha = value;
+    })
+    .addBoolean("fill", fill, function(value) {
+        fill = value;
+    })
+   .addButton("randomize", function() {
+        panel.setValue("scale", bitlib.random.float(0.4, 0.7));
+        panel.setValue("dist", bitlib.random.float(20, 300));
+        panel.setValue("w", bitlib.random.float(20, 300));
+        panel.setValue("h", bitlib.random.float(20, 300));
+        panel.setValue("textRotation", bitlib.random.float(Math.PI * 2));
+    })
+    .setGlobalChangeHandler(start);
+
+var anglePanel = QuickSettings.create(width - 220, 20, "Angles")
+    .addRange("angle 0", 0, Math.PI * 2, angles[0], 0.01, function(value) {
+        angles[0] = value;
+    })
+    .addRange("angle 1", 0, Math.PI * 2, angles[1], 0.01, function(value) {
+        angles[1] = value;
+    })
+    .addRange("angle 2", 0, Math.PI * 2, angles[2], 0.01, function(value) {
+        angles[2] = value;
+    })
+    .addRange("angle 3", 0, Math.PI * 2, angles[3], 0.01, function(value) {
+        angles[3] = value;
+    })
+    .addButton("randomize", function() {
+        anglePanel.setValue("angle 0", bitlib.random.float(0, Math.PI * 2));
+        anglePanel.setValue("angle 1", bitlib.random.float(0, Math.PI * 2));
+        anglePanel.setValue("angle 2", bitlib.random.float(0, Math.PI * 2));
+        anglePanel.setValue("angle 3", bitlib.random.float(0, Math.PI * 2));
+    })
+    .setGlobalChangeHandler(start);
+
+start();
+
+function start() {
+    context.clear("white");
+    context.font = fontSize + "px Arial";
+    context.save();
+    context.translate(width / 2, height / 2);
+    context.globalAlpha = alpha;
+    draw(iter);
+    context.restore();
+}
+
+function draw(iter) {
+    for(var i = 0; i < num; i++) {
+        context.save();
+        context.rotate(angles[i]);
+        context.translate(dist, 0);
+        context.rotate(textRotation);
+        if(fill) {
+            context.fillText(text, 0, 0);
+        }
+        else {
+            context.strokeText(text, 0, 0);
+        }
+        if(iter) {
+            context.scale(scale, scale);
+            draw(iter - 1);
+        }
+        context.restore();
+    }
+}
